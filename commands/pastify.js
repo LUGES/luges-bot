acfb0ca8f330b12926fa0a994c489b5dc0f86062
Brent Oates <b.oates@lancaster.ac.uk>
2019-08-04 17:47:37 +0100

    Added verification to pastify

--- commands/pastify.js
+++ commands/pastify.js
@@ -7,15 +7,33 @@ exports.run = (client, message) => {
     if(currentMemberRole == undefined || pastMemberRole == undefined)
         return (message.channel.send("Unable to locate the Member roles."));
 
+    message.channel.send({embed: {
+        color: 1037917,
+        title: "Member Reassignment",
+        description: "Type '*confirm' within 10 Seconds to proceed",
+        fields: [{name: "Members to reassign", value: message.guild.members.filter(m => m.roles.find(role => role.name == "Current Member")).size, inline : true}]
+    }});
 
-    message.guild.fetchMembers().then(() => {
-        let enrolled = message.guild.members.filter(m => m.roles.find(role => role.name == "Current Member"));
-        enrolled.forEach(member => {
-            member.removeRole(currentMemberRole)
-                .then(member.addRole(pastMemberRole));
-            
+    const collector = new client.Discord.MessageCollector(message.channel, m => m.content == "*confirm" && m.author.id === message.author.id, { time: 10000 });
+
+    collector.on("collect", message => {
+        message.channel.send("Refresh confirmed, reassigning Member roles.");
+        message.guild.fetchMembers().then(() => {
+            let enrolled = message.guild.members.filter(m => m.roles.find(role => role.name == "Current Member"));
+            enrolled.forEach(member => {
+                member.removeRole(currentMemberRole)
+                    .then(member.addRole(pastMemberRole));
+                
+            });
         });
+        collector.stop();
     });
+
+    collector.on("end", (msgs) => {
+        if (msgs.size == 0)
+            message.channel.send("No confirmation, operation halted.");
+    });
+    
 
     if (message.channel.type != "dm")
         message.delete();
